ed047a09acd82032e8519e2f3acb2a8cb3568536
kazu_wsx <kazu.wsx@gmail.com>
2018-03-31 18:17:40 +0900

    addAutomaticUpdatesFeatureFunction

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,13 +1,14 @@
 $(function() {
   function buildHTML(message){
-    if( message.image_tag) {
-      var img = `<img src='${message.image_tag}' class="lower-message__image">`
-    } else{
-      var img = ''
+    if( message.content !== undefined) {
+      var con = <p class="lower-message__content">${message.content}</p>
+    }
+    if( message.image_tag !== undefined) {
+      var img = <img src='${message.image_tag}' >
     }
    var html =
    `
-  <div class="message" data-id="${message.id}">
+  <div class="message">
     <div class=upper-message>
       <div class="upper-message__user-name">
         ${message.user_name}
@@ -17,16 +18,13 @@ $(function() {
       </div>
     </div>
     <div lower-message>
-      <p class="lower-message__content">
-        ${message.content}
-      </p>
+      ${con}
       ${img}
     </div>
-
   <div>
-       `
+       ` 
    return html;
-  }
+ }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -49,57 +47,4 @@ $(function() {
       alert('投稿に失敗しました');
     })
   });
-
-
-  ////////////////////////////////自動更新機能//////////////////////////////////////////////////////////////////
-  setInterval(function() {
-    var href = window.location.href;
-    $.ajax({
-      url: href,
-      dataType:'json',
-      type:'GET',
-    })
-    .done(function(data) { //.done(function(json){})でjbuilderから受け取ったデータを引数dataで使用
-      var insertHTML = '';//メッセージを複数入れるための変数。空の状態で定義。
-      var id = $('.message').last().attr('data-id');//表示中のメッセージのうち最新のidを取得
-
-      data.messages.forEach(function(message) {//jsonで定義したjson.messagesをひとつひとつforEachで処理
-        if (message.id > id ) {
-          var html = newMessageHTML(message);
-          $('.messages').append(html);
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        }
-      });
-    })
-    .fail(function(data) {
-      alert('自動更新に失敗しました');
-    });
-  } , 5 * 1000 );
-
-  function newMessageHTML(message) {
-    var insertImage = '';
-    if (message.image) {
-    insertImage = `<img src="${message.image}">`;
-    }
-    var html =
-      `
-      <div class="message" data-id="${message.id}">
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${ message.user_name }
-          </div>
-          <div class="upper-message__date">
-            ${ message.created_at }
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${ message.content }
-          </p>
-          ${insertImage}
-        </div>
-      </div>
-      `
-    return html
-  }
 });
